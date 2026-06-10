a5d18a27b4240e6aaf7bdbb5b038c5349cd426dc
Yehudit2005 <y0777500720@gmail.com>
2026-06-10 23:47:17 +0300

    הוספת חיפושים בצד השרת

--- src/components/InstructorTasks/InstructorTasks.tsx
+++ src/components/InstructorTasks/InstructorTasks.tsx
@@ -5,46 +5,47 @@ import type { RootState } from '../../store/store';
 import type { TeacherTask } from '../../Models/teacherTask.model';
 import InstructorTask from '../InstructorTasks/InstructorTask/InstructorTask';
 
-
 interface InstructorTasksProps { }
 const allJson = 'http://localhost:3001';
 
 const InstructorTasks: FC<InstructorTasksProps> = () => {
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
   const [tasks, setTasks] = useState<TeacherTask[]>([]);
-  const [search, setSearch] = useState('');
+  const [searchTask, setSearchTask] = useState('');
+  const [searchStudent, setSearchStudent] = useState('');
 
   useEffect(() => {
     if (!currentUser) return;
 
     const fetchTasks = async () => {
-  // const url = search
-  //   ? `${allJson}/pending_reviews?instructor_id=${currentUser.id}&task_title_like=${search}`
-  //   : `${allJson}/pending_reviews?instructor_id=${currentUser.id}`;
-  const url = search
-  ? `${allJson}/pending_reviews?instructor_id=${currentUser.id}&q=${search}`
-  : `${allJson}/pending_reviews?instructor_id=${currentUser.id}`;
-    
-  const res = await fetch(url);
-  const data = await res.json();
-  setTasks(data);
-};
+      let url = `${allJson}/pending_reviews?instructor_id=${currentUser.id}`;
+      if (searchTask) url += `&task_title_like=${searchTask}`;
+      if (searchStudent) url += `&student_name_like=${searchStudent}`;
+
+      const res = await fetch(url);
+      const data = await res.json();
+      setTasks(data);
+    };
 
     fetchTasks();
-  }, [currentUser, search]);
+  }, [currentUser, searchTask, searchStudent]);
 
   return (
     <div>
       <input
-        placeholder="חיפוש משימה..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        placeholder="חיפוש לפי כותרת משימה..."
+        value={searchTask}
+        onChange={(e) => setSearchTask(e.target.value)}
+      />
+      <input
+        placeholder="חיפוש לפי שם תלמיד..."
+        value={searchStudent}
+        onChange={(e) => setSearchStudent(e.target.value)}
       />
       {tasks.map((t) => (
         <InstructorTask key={t.id} task={t} />
       ))}
     </div>
   );
-};
-
+}
 export default InstructorTasks;
